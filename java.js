29705218517a0e1fd2532de8cafa73e497d27723
abidasakk <tanhaxelkevin@gmail.com>
2023-01-27 13:54:41 +0100

    exercice sur le DOM

---
+++ java.js
@@ -0,0 +1,7 @@
+let btn= document.querySelector('.on');
+btn.addEventListener('click', lumiere);
+
+function lumiere(){
+let light=document.querySelector('.light');
+light.setAttribute('src', 'abdaalu.jpg')   
+}
